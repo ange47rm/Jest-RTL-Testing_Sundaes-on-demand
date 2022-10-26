68d30bd843e6099ec428c12ee5b78cf355f36600
ange47rm <ange47rm@hotmai.com>
2022-10-26 19:09:55 +0100

    End of day 9 commit. Alerts tests passing. Mocked server errors.

---
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
+
+test("handles error for scoops and toppings routes", async () => {
+  // Server handlers can be overridden
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
+      res(ctx.status(500))
+    ),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
+      res(ctx.status(500))
+    )
+  );
+
+  render(<OrderEntry />);
+
+  // The error are going to appear asynchronously, after the promise is rejected (Options.jsx - Line 15)
+  // Older/Slower PCs may render alerts more slowly than faster PCs, which means the expect assertion below will fail
+  // as it'll find one alert, instead of two, as the second one has not yet been rendered.
+  // To ensure that all elements are rendered before making the assertion, we can use waitFor
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
+  });
+});
